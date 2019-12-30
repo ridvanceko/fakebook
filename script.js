928a2765872cbca1500adc7c74e8562b641fc0af
Ridvan Cekmez <ridvanceko@gmail.com>
2019-12-29 20:53:03 -0700

    practice

--- script.js
+++ script.js
@@ -2,6 +2,14 @@ var database = [
   {
     username: "ceko",
     password: "secret"
+  },
+  {
+    username: "fero",
+    password: "secret1"
+  },
+  {
+    username: "muho",
+    password: "secret2"
   }
 ];
 
@@ -13,17 +21,37 @@ var newsFeed = [
   {
     username: "Ridvan",
     timeline: "JS is the best language"
+  },
+  {
+    username: "Miley",
+    timeline: "I am the cutest"
   }
 ];
 
-var userNamePrompt = prompt("What is your username?");
-var passwordPrompt = prompt("What is your password?");
 
-function signIn(user, pass) {
-  if (user === database[0].username && pass === database[0].password) {
-    console.log(newsFeed);
+
+
+function isUserValid(username, password){
+  for (var i=0; i < database.length; i++) {
+    if (database[i].username === username && database[i].password === password) {
+      return true;
+    }
+    }
+
+    return false;
+  }
+
+
+function signIn(username, password) {
+  
+  
+  if (isUserValid(username, password)) {
   } else {
     alert("Sorry, wrong username and password");
   }
 }
+
+var userNamePrompt = prompt("What is your username?");
+var passwordPrompt = prompt("What is your password?");
+
 signIn(userNamePrompt, passwordPrompt);
